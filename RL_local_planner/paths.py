3de6a6aa298b8dc9f5b73d55dfe113a09584ee4b
harshnehal1996 <harshnehal1996@gmail.com>
2023-10-19 23:47:45 +0200

    refactored code

--- RL_local_planner/paths.py
+++ RL_local_planner/paths.py
@@ -6,4 +6,4 @@ class ProjectPaths:
 	expert_directory = os.path.join(project_path, 'collected_trajectories/')
 	grid_dir = os.path.join(project_path, 'cache/image.png')
 	path_to_save = os.path.join(project_path, 'weights/both/')
-	carla_pylibs = '/home/harsh/Documents/carla_sim/carla/PythonAPI/carla/dist/carla-0.9.10-py3.6-linux-x86_64.egg'
+	carla_pylibs = 'path/to/carla_pylibs.egg' # example : '/home/user/carla_sim/carla/PythonAPI/carla/dist/carla-0.9.10-py3.6-linux-x86_64.egg'
